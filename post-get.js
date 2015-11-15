492e151d85bd82ed6cc669469fcfba90dbdc0d3c
kommissarnicko <kommissarnicko@gmail.com>
2015-11-14 22:27:57 -0700

    Initial setup

---
+++ post-get.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+app.get('/',function(req,res){
+  res.render('home')
+});
+
+
+function genContext(){
+  var stuffToDisplay = {};
+  stuffToDisplay.time = (new Date(Date.now())).toLocaleTimeString('en-US');
+  return stuffToDisplay;
+}
+
+app.get('/get', function(req, res) 
+{
+	var getArray = [];
+	for (var i in req.query)
+	{
+		getArray.push({'name':i, 'value':req.query[i]})
+	}
+	var context = {};
+	context.dataTable = getArray;
+	res.render('get', context);
+});
+
