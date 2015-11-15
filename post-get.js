ef2c27d180eea871d91254b448f7065bca6dea1d
kommissarnicko <kommissarnicko@gmail.com>
2015-11-14 22:42:48 -0700

    error fixing

--- post-get.js
+++ post-get.js
@@ -11,32 +11,9 @@ app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
-});
-
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.render('500');
-});
-
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
-
-app.get('/',function(req,res){
-  res.render('home')
-});
-
-
-function genContext(){
-  var stuffToDisplay = {};
-  stuffToDisplay.time = (new Date(Date.now())).toLocaleTimeString('en-US');
-  return stuffToDisplay;
-}
 
 app.get('/get', function(req, res) 
 {
@@ -50,3 +27,14 @@ app.get('/get', function(req, res)
 	res.render('get', context);
 });
 
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
